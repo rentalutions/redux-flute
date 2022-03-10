00fdbb76dace22638c9d1ee6e177664d9d4b003e
Dmytro Bubon <dmytrob@avail.co>
2022-03-10 16:54:09 +0200

    Remove docs and examples

--- examples/todos/src/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"
-import flute, { reducer as models } from "./redux-flute"
-flute.setAPI({ prefix: "https://jsonplaceholder.typicode.com" });
-import "./models"
-export default combineReducers({
-  models
-});
--- examples/todos/src/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import reducer from "./reducers";
-import { middleware as fluteMiddleware } from "./redux-flute";
-export default createStore(reducer, compose(applyMiddleware(fluteMiddleware)));
